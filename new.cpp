00370c2c572bd32d7b634340f1489c18b3c34c6e
Pragyan Kantiwal <Pragyankantiwal1234@gmail.com>
2024-11-12 12:53:23 +0530

    This is v2 of new.cpp

--- new.cpp
+++ new.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
-
+class Git{
+    string a;
+public:
+    void getter(){
+        cout<<"Enter your name: "<<endl;
+        cin>>a;
+        cout<<"Your name is " << a;
+    }
+    
+};
 
 int main(){
-    printf("HEllo dosto hum sikh rahe h git version control.");
+    printf("HEllo dosto hum sikh rahe h git version control.\n");
+    Git V2;
+    V2.getter();
     return 0;
 }
